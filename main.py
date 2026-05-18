2bc497fceb4a01b61117dedf5b6d0c9b4890b3f1
Igor Gashenko <igor_4000@mail.ru>
2026-05-18 13:22:40 +0300

    Initial commit

--- main.py
+++ main.py
@@ -1,15 +1,27 @@
 class FlatIterator:
 
     def __init__(self, list_of_list):
-        ...
+        self.list_of_list = list_of_list
+        self.outer_index = 0
+        self.inner_index = 0
+
 
     def __iter__(self):
-        ...
+        self.outer_index = 0
+        self.inner_index = 0
         return self
 
     def __next__(self):
-        ...
-        return item
+        while self.outer_index < len(self.list_of_list):
+            inner_list = self.list_of_list[self.outer_index]
+            if self.inner_index < len(inner_list):
+                item = inner_list[self.inner_index]
+                self.inner_index += 1
+                return item
+            self.outer_index += 1
+            self.inner_index = 0
+        
+        raise StopIteration
 
 
 def test_1():
@@ -28,7 +40,7 @@ def test_1():
         assert flat_iterator_item == check_item
 
     assert list(FlatIterator(list_of_lists_1)) == ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None]
-
+    print(list(FlatIterator(list_of_lists_1)))
 
 if __name__ == '__main__':
     test_1()
